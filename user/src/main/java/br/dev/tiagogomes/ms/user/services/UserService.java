7d0ee19d1a52364a401493e0ab6564cd3e7ffb99
Tiago Gomes <tiagogomes187@gmail.com>
2024-01-06 23:42:54 -0300

    UserProducer

--- user/src/main/java/br/dev/tiagogomes/ms/user/services/UserService.java
+++ user/src/main/java/br/dev/tiagogomes/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package br.dev.tiagogomes.ms.user.services;
 
 import br.dev.tiagogomes.ms.user.models.UserModel;
+import br.dev.tiagogomes.ms.user.producers.UserProducer;
 import br.dev.tiagogomes.ms.user.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,14 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 
 }
